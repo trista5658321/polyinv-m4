230cd61f9180a75c573b1a9a513cf93d32980276
Trista Li <trista5658321@gmail.com>
2021-01-23 17:00:27 +0800

    update jump32 speed (2x2)

--- parse_jump/change_param_order.py
+++ parse_jump/change_param_order.py
@@ -1,6 +1,6 @@
 import pathlib
 
-LENGTH = 16
+LENGTH = 32
 FILE_NAME = "jump" + str(LENGTH) + "_2x2"
 
 # For the directory of the script being run:
@@ -11,11 +11,12 @@ new_f = open(PATH_DIR.joinpath("new_" + FILE_NAME + ".S"), 'w')
 code_list = f.readlines()
 for code in code_list:
     if "r1," in code or "r2," in code:
-        pos = code.index("[")
-        code_l = code[:pos+1]
-        code_r = code[pos+3:]
-        reg_name = ["r1", "r2"]["r1," in code]
-        new_f.write(code_l + reg_name + code_r)
+        reg_name = ["r2", "r1"]["r1," in code]
+        pos = code.index(reg_name)
+        code_l = code[:pos]
+        code_r = code[pos+2:]
+        new_reg_name = ["r2", "r1"]["r2" == reg_name]
+        new_f.write(code_l + new_reg_name + code_r)
     else:
         new_f.write(code)
 
