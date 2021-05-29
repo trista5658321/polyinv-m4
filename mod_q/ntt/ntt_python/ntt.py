0fe71f12718410dc6d43d21c0d3a3368cfd31b39
Trista Li <trista5658321@gmail.com>
2021-05-29 18:21:22 +0800

    test ntt(x)

--- mod_q/ntt/ntt_python/ntt.py
+++ mod_q/ntt/ntt_python/ntt.py
@@ -6,26 +6,26 @@ def reduce_center(p,x):
         x+=p
     return x
 
-def butterfly(p, poly_len, f, c, shift): # f +- cg
+def butterfly(p, poly_len, F, f, c, shift, layer_id): # f +- cg
+    input_f = F
+    if layer_id == 0:
+        input_f = f
     for i in range(poly_len):
-        _f = f[i+shift]
-        _g = c*f[i+poly_len+shift]
-        # print("f[%d] = %d" % (i+shift, _f))
-        # print("f'[%d] = %d" % (i+poly_len+shift, _g))
-        # print("(_f + _g) mod p = (%d + %d)" % (_f, _g))
-        f[i+shift] = reduce_center(p, _f + _g)
-        f[i+poly_len+shift] = reduce_center(p, _f - _g)
+        _f = input_f[i+shift]
+        _g = c*input_f[i+poly_len+shift]
+        F[i+shift] = reduce_center(p, _f + _g)
+        F[i+poly_len+shift] = reduce_center(p, _f - _g)
 
-def ntt(n, layer, p, w, c, f):
+def ntt(n, layer, p, w, c, F, f):
     poly_len = n
     for i in range(layer):
         poly_len //= 2
         times = pow(2, i)
-        # print(f)
+        # print(F)
         # print("= %d =" % (i))
         # print("-> %d %d" % (poly_len, times))
         for j in range(times):
             _c = c[i][j]
             shift = j * poly_len*2
             # print("shift = %d" % (shift))
-            butterfly(p, poly_len, f, _c, shift)
+            butterfly(p, poly_len, F, f, _c, shift, i)
