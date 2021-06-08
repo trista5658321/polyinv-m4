ef6e7c1b386dbb58f139af7914440b61b82fb593
Trista Li <trista5658321@gmail.com>
2021-06-09 01:49:52 +0800

    gen jumpdivstep [big prime ntt] for 1277

--- mod_q/gen_divstep.sh
+++ mod_q/gen_divstep.sh
@@ -4,11 +4,13 @@
 python gen_jumpdivsteps/gen_n_full.py 256 5167 > jumpdivstep/big_prime_ntt/sntrup857/jump256divsteps.c
 python gen_jumpdivsteps/gen_n_full.py 256 6343 > jumpdivstep/big_prime_ntt/sntrup953/jump256divsteps.c
 python gen_jumpdivsteps/gen_n_full.py 256 7177 > jumpdivstep/big_prime_ntt/sntrup1013/jump256divsteps.c
+python gen_jumpdivsteps/gen_n_full.py 256 7879 > jumpdivstep/big_prime_ntt/sntrup1277/jump256divsteps.c
 
 # jump 512
 python gen_jumpdivsteps/gen_n_full.py 512 5167 > jumpdivstep/big_prime_ntt/sntrup857/jump512divsteps.c
 python gen_jumpdivsteps/gen_n_full.py 512 6343 > jumpdivstep/big_prime_ntt/sntrup953/jump512divsteps.c
 python gen_jumpdivsteps/gen_n_full.py 512 7177 > jumpdivstep/big_prime_ntt/sntrup1013/jump512divsteps.c
+python gen_jumpdivsteps/gen_n_full.py 512 7879 > jumpdivstep/big_prime_ntt/sntrup1277/jump512divsteps.c
 
 # jump 1024
 # python gen_jumpdivsteps/.py 1024 5167 > jumpdivstep/big_prime_ntt/sntrup857/jump1024divsteps.c
@@ -17,56 +19,6 @@ python gen_jumpdivsteps/gen_1024.py 1024 7177 > jumpdivstep/big_prime_ntt/sntrup
 
 
 
-
-
-
-
-
-
-
-
-
 # === v1 ===
 python v_1/gen_recip/gen_divsteps_653.py 512 653 656 > v_1/final/sntrup653/jump656divsteps.c
-
-
-
-# 128
-# python gen_polyinverse/gen.py 128 653 656 > final/sntrup653/jump1312divsteps_mod3_128.c
-# python polymul/gen_mul.py 128 653 656 > final/sntrup653/mod3_mul128xN.S
-# python polymul/gen_mul_last.py 32 653 656 > final/sntrup653/mod3_mul32xN.S
-# python gen_update_fg.py 128 653 656 > final/sntrup653/mod3_update_fg_128.S
-# python gen_update_VS.py 128 653 656 > final/sntrup653/mod3_update_VS_128.S
-
 python v_2/gen_polyinverse/gen_divsteps.py 128 761 768 > v_2/assembly/sntrup761/jump1536divsteps_128.c
-
-# python polymul/gen_mul.py 128 761 768 > final/sntrup761/mod3_mul128xN.S
-# python gen_update_fg.py 128 761 768 > final/sntrup761/mod3_update_fg_128.S
-# python gen_update_VS.py 128 761 768 > final/sntrup761/mod3_update_VS_128.S
-
-# python gen_polyinverse/gen.py 128 857 864 > final/sntrup857/jump1728divsteps_mod3_128.c
-# python polymul/gen_mul.py 128 857 864 > final/sntrup857/mod3_mul128xN.S
-# python polymul/gen_mul_last.py 64 857 864 > final/sntrup857/mod3_mul64xN_128.S
-# python gen_update_fg.py 128 857 864 > final/sntrup857/mod3_update_fg_128.S
-# python gen_update_VS.py 128 857 864 > final/sntrup857/mod3_update_VS_128.S
-
-# python gen_polyinverse/gen.py 128 953 960 > final/sntrup953/jump1920divsteps_mod3_128.c
-# python polymul/gen_mul.py 128 953 960 > final/sntrup953/mod3_mul128xN.S
-# python gen_update_fg.py 128 953 960 > final/sntrup953/mod3_update_fg_128.S
-# python gen_update_VS.py 128 953 960 > final/sntrup953/mod3_update_VS_128.S
-
-# python gen_polyinverse/gen.py 128 1013 1024 > final/sntrup1013/jump2048divsteps_mod3_128.c
-# python polymul/gen_mul.py 128 1013 1024 > final/sntrup1013/mod3_mul128xN.S
-# python gen_update_fg.py 128 1013 1024 > final/sntrup1013/mod3_update_fg_128.S
-# python gen_update_VS.py 128 1013 1024 > final/sntrup1013/mod3_update_VS_128.S
-
-# python gen_polyinverse/gen.py 128 1277 1280 > final/sntrup1277/jump2560divsteps_mod3_128.c
-# python polymul/gen_mul.py 128 1277 1280 > final/sntrup1277/mod3_mul128xN.S
-# python gen_update_fg.py 128 1277 1280 > final/sntrup1277/mod3_update_fg_128.S
-# python gen_update_VS.py 128 1277 1280 > final/sntrup1277/mod3_update_VS_128.S
-
-# # 256
-# python gen_polyinverse/gen.py 256 1013 1024 > final/sntrup1013/jump2048divsteps_mod3_256.c
-# python polymul/gen_mul.py 256 1013 1024 > final/sntrup1013/mod3_mul256xN.S
-# python gen_update_fg.py 256 1013 1024 > final/sntrup1013/mod3_update_fg_256.S
-# python gen_update_VS.py 256 1013 1024 > final/sntrup1013/mod3_update_VS_256.S
---
+++ mod_q/jumpdivstep/big_prime_ntt/sntrup1277/jump512divsteps.c
@@ -0,0 +1,200 @@
+#include <stdint.h>
+#include "cmsis.h"
+#include <stdio.h>
+
+extern int jump256divsteps(int minusdelta, int *M, int *f, int *g);
+void __gf_polymul_256x256_2x2_x2p2 (int *V, int *M_16, int *M_32, int *fh, int *gh, int *M);
+void __gf_polymul_256x256_2x2_x_2x2 (int * M, int * M1_16, int * M1_32, int * M2_16, int * M2_32);
+int jump512divsteps(int minusdelta, int *M, int *f, int *g);
+
+#define q 7879
+#define qR2inv 545116 // round(2^32/q)
+#define _2P15 (1 << 15)
+
+#if 1
+// result range: +- 3939 (note: 3 loads for _2P15 and the longer qR2inv)
+static inline int barrett_16x2i(int X) {
+  int32_t QL = __SMLAWB(qR2inv,X,_2P15);
+  int32_t QH = __SMLAWT(qR2inv,X,_2P15);
+  int32_t SL = __SMULBT(q,QL);
+  int32_t SH = __SMULBT(q,QH);
+  return(__SSUB16(X,__PKHBT(SL,SH,16)));
+}
+
+#else 
+#define barrett_16x2i(A) (A)
+#endif
+
+void __gf_polymul_256x256_2x2_x_2x2(int * M, int * M1_16, int * M1_32, int * M2_16, int * M2_32){
+  int tmp_16_1[256];
+  int tmp_32_0[512], tmp_32_1[512];
+  int i, T, *X, *Y;
+
+  /* u */
+  basemul512_16bit_4x4(M, M2_16, M1_16); // uux
+  basemul512_16bit_4x4(tmp_16_1, M2_16+256, M1_16+512); // vr
+  basemul512_32bit_4x4(tmp_32_0, M2_32, M1_32); // uux
+  basemul512_32bit_4x4(tmp_32_1, M2_32+512, M1_32+1024); // vr
+  for (X=M, Y=tmp_16_1, i=256; i>0; i--) {
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(M);
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+  crt512(M, tmp_32_0);
+
+  /* v */
+  basemul512_16bit_4x4(M+256, M2_16, M1_16+256); // uvx
+  basemul512_16bit_4x4(tmp_16_1, M2_16+256, M1_16+768); // vs
+  basemul512_32bit_4x4(tmp_32_0, M2_32, M1_32+512); // uvx
+  basemul512_32bit_4x4(tmp_32_1, M2_32+512, M1_32+1536); // vs
+  for (X=M+256, Y=tmp_16_1, i=256; i>0; i--) {  
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(M+256);
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+  crt512(M+256, tmp_32_0);
+
+  /* r */
+  basemul512_16bit_4x4(M+512, M2_16+512, M1_16); // r2 u1 x
+  basemul512_16bit_4x4(tmp_16_1, M2_16+768, M1_16+512); // s2 r1
+  basemul512_32bit_4x4(tmp_32_0, M2_32+1024, M1_32); // r2 u1 x
+  basemul512_32bit_4x4(tmp_32_1, M2_32+1536, M1_32+1024); // s2 r1
+  for (X=M+512, Y=tmp_16_1, i=256; i>0; i--) {
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(M+512);
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+  crt512(M+512, tmp_32_0);
+
+  /* s */
+  basemul512_16bit_4x4(M+768, M2_16+512, M1_16+256); // rvx
+  basemul512_16bit_4x4(tmp_16_1, M2_16+768, M1_16+768); // ss
+  basemul512_32bit_4x4(tmp_32_0, M2_32+1024, M1_32+512); // rvx
+  basemul512_32bit_4x4(tmp_32_1, M2_32+1536, M1_32+1536); // ss
+  for (X=M+768, Y=tmp_16_1, i=256; i>0; i--) {
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(M+768);
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+  crt512(M+768, tmp_32_0);
+}
+
+void __gf_polymul_256x256_2x2_x2p2(int *V, int *M_16, int *M_32, int *fh, int *gh, int *M){
+  int fh_16[256], gh_16[256];
+  int fh_32[512], gh_32[512];
+  int tmp_16_1[256];
+  int tmp_32_0[512], tmp_32_1[512];
+
+  basemul_x_512_16bit_4x4(M_16); // u x
+  basemul_x_512_16bit_4x4(M_16+256); // v x
+  basemul_x_512_32bit_4x4(M_32); // u x
+  basemul_x_512_32bit_4x4(M_32+512); // v x
+
+  ntt512_16bit(fh_16, fh);
+  ntt512_32bit(fh_32, fh);
+  ntt512_16bit(gh_16, gh);
+  ntt512_32bit(gh_32, gh);
+
+  basemul512_16bit_4x4(V, M_16, fh_16); // ux * fh
+  basemul512_16bit_4x4(tmp_16_1, M_16+256, gh_16); // vx * gh
+  basemul512_32bit_4x4(tmp_32_0, M_32, fh_32); // ux * fh
+  basemul512_32bit_4x4(tmp_32_1, M_32+512, gh_32); // vx * gh
+
+  int i, T, *X, *Y, *W;
+  for (X=V, Y=tmp_16_1, i=256; i>0; i--) {
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(V);
+
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+
+  crt512(V, tmp_32_0);
+
+  basemul512_16bit_4x4(V+256, M_16+512, fh_16); // r * fh
+  basemul512_16bit_4x4(tmp_16_1, M_16+768, gh_16); // s * gh
+  basemul512_32bit_4x4(tmp_32_0, M_32+1024, fh_32); // r * fh
+  basemul512_32bit_4x4(tmp_32_1, M_32+1536, gh_32); // s * gh
+
+  for (X=V+256, Y=tmp_16_1, i=256; i>0; i--) {
+    T = __SADD16(*(Y++),*X);
+    *(X++) = T;
+  }
+  intt512_16bit(V+256);
+
+  for (X=tmp_32_0, Y=tmp_32_1, i=512; i>0; i--) {
+    T = *(Y++) + *X;
+    *(X++) = T;
+  }
+  intt512_32bit(tmp_32_0);
+
+  crt512(V+256, tmp_32_0);
+
+  for (X=V, Y=M, i=128; i>0; i--) {  // + f'
+    T = barrett_16x2i(__SADD16(*(Y++),*X)); *(X++) = T;
+  } 
+  for (X=V+256, Y=M+128, i=128; i>0; i--) {  // + g'
+    T = barrett_16x2i(__SADD16(*(Y++),*X)); *(X++) = T;
+  }
+}
+
+int jump512divsteps(int minusdelta, int *M, int *f, int *g){
+  int M1[768], M2[768], fg[512];
+  int M1_16[1024]={0}, M1_32[2048];
+  int M2_16[1024]={0}, M2_32[2048];
+
+  minusdelta = jump256divsteps(minusdelta, M1, f, g);
+
+  ntt512_16bit(M1_16, M1+256); // u1
+  ntt512_16bit(M1_16+256, M1+384); // v1
+  ntt512_16bit(M1_16+512, M1+512); // r1
+  ntt512_16bit(M1_16+768, M1+640); // s1
+
+  ntt512_32bit(M1_32, M1+256); // u1
+  ntt512_32bit(M1_32+512, M1+384); // v1
+  ntt512_32bit(M1_32+1024, M1+512); // r1
+  ntt512_32bit(M1_32+1536, M1+640); // s1
+
+  __gf_polymul_256x256_2x2_x2p2(fg, M1_16, M1_32, f+128, g+128, M1);
+
+  minusdelta = jump256divsteps(minusdelta, M2, fg, fg+256);
+
+  ntt512_16bit(M2_16, M2+256); // u2
+  ntt512_16bit(M2_16+256, M2+384); // v2
+  ntt512_16bit(M2_16+512, M2+512); // r2
+  ntt512_16bit(M2_16+768, M2+640); // s2
+
+  ntt512_32bit(M2_32, M2+256); // u2
+  ntt512_32bit(M2_32+512, M2+384); // v2
+  ntt512_32bit(M2_32+1024, M2+512); // r2
+  ntt512_32bit(M2_32+1536, M2+640); // s2
+
+  __gf_polymul_256x256_2x2_x_2x2(M+512, M1_16, M1_32, M2_16, M2_32);
+  __gf_polymul_256x256_2x2_x2p2(M, M2_16, M2_32, fg+128, fg+384, M2);
+
+  return(minusdelta);
+}
