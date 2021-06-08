e1aaba0b6f5ee629bf7815a26a82a7c4aabaff54
Trista Li <trista5658321@gmail.com>
2021-06-09 01:21:01 +0800

    sch is faster

--- mod_q/jumpdivstep/sntrup1013/jump256divsteps.c
+++ mod_q/jumpdivstep/sntrup1013/jump256divsteps.c
@@ -2,9 +2,10 @@
 #include "cmsis.h"
 #include <stdio.h>
 
+extern void gf_polymul_128x128(int *h, int *f, int *g);
 extern int jump128divsteps(int minusdelta, int *M, int *f, int *g);
-void __gf_polymul_128x128_2x2_x2p2 (int *V, int *M_16, int *M_32, int *fh, int *gh, int *M);
-void __gf_polymul_128x128_2x2_x_2x2 (int * M, int * M1_16, int * M1_32, int * M2_16, int * M2_32);
+void gf_polymul_128x128_2x2_x2p2 (int *V,int *M,int *fh,int *gh);
+void gf_polymul_128x128_2x2_x_2x2 (int *M, int *M1, int *M2);
 int jump256divsteps(int minusdelta, int *M, int *f, int *g);
 
 #define q 7177
@@ -12,7 +13,7 @@ int jump256divsteps(int minusdelta, int *M, int *f, int *g);
 #define _2P15 (1 << 15)
 
 #if 1
-// result range: +- 3588 (note: 3 loads for _2P15 and the longer qR2inv)
+// result range: +- 2295 (note: 3 loads for _2P15 and the longer qR2inv)
 static inline int barrett_16x2i(int X) {
   int32_t QL = __SMLAWB(qR2inv,X,_2P15);
   int32_t QH = __SMLAWT(qR2inv,X,_2P15);
@@ -24,177 +25,64 @@ static inline int barrett_16x2i(int X) {
 #else 
 #define barrett_16x2i(A) (A)
 #endif
+//static
+int B256_1[129];
+int * BB256_1 = (int *)((void *)B256_1 + 2);
 
-void __gf_polymul_128x128_2x2_x_2x2(int * M, int * M1_16, int * M1_32, int * M2_16, int * M2_32){
-  int tmp_16_1[128];
-  int tmp_32_0[256], tmp_32_1[256];
-  int i, T, *X, *Y;
-
-  /* u */
-  basemul256_16bit_2x2(M, M2_16, M1_16); // uux
-  basemul256_16bit_2x2(tmp_16_1, M2_16+128, M1_16+256); // vr
-  basemul256_32bit_2x2(tmp_32_0, M2_32, M1_32); // uux
-  basemul256_32bit_2x2(tmp_32_1, M2_32+256, M1_32+512); // vr
-  for (X=M, Y=tmp_16_1, i=128; i>0; i--) {
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(M);
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-  crt256(M, tmp_32_0);
-
-  /* v */
-  basemul256_16bit_2x2(M+128, M2_16, M1_16+128); // uvx
-  basemul256_16bit_2x2(tmp_16_1, M2_16+128, M1_16+384); // vs
-  basemul256_32bit_2x2(tmp_32_0, M2_32, M1_32+256); // uvx
-  basemul256_32bit_2x2(tmp_32_1, M2_32+256, M1_32+768); // vs
-  for (X=M+128, Y=tmp_16_1, i=128; i>0; i--) {  
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(M+128);
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-  crt256(M+128, tmp_32_0);
-
-  /* r */
-  basemul256_16bit_2x2(M+256, M2_16+256, M1_16); // r2 u1 x
-  basemul256_16bit_2x2(tmp_16_1, M2_16+384, M1_16+256); // s2 r1
-  basemul256_32bit_2x2(tmp_32_0, M2_32+512, M1_32); // r2 u1 x
-  basemul256_32bit_2x2(tmp_32_1, M2_32+768, M1_32+512); // s2 r1
-  for (X=M+256, Y=tmp_16_1, i=128; i>0; i--) {
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(M+256);
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-  crt256(M+256, tmp_32_0);
-
-  /* s */
-  basemul256_16bit_2x2(M+384, M2_16+256, M1_16+128); // rvx
-  basemul256_16bit_2x2(tmp_16_1, M2_16+384, M1_16+384); // ss
-  basemul256_32bit_2x2(tmp_32_0, M2_32+512, M1_32+256); // rvx
-  basemul256_32bit_2x2(tmp_32_1, M2_32+768, M1_32+768); // ss
-  for (X=M+384, Y=tmp_16_1, i=128; i>0; i--) {
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(M+384);
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-  crt256(M+384, tmp_32_0);
-}
-
-void __gf_polymul_128x128_2x2_x2p2(int *V, int *M_16, int *M_32, int *fh, int *gh, int *M){
-  int fh_16[128], gh_16[128];
-  int fh_32[256], gh_32[256];
-  int tmp_16_1[128];
-  int tmp_32_0[256], tmp_32_1[256];
-
-  basemul_x_256_16bit_2x2(M_16); // u x
-  basemul_x_256_16bit_2x2(M_16+128); // v x
-  basemul_x_256_32bit_2x2(M_32); // u x
-  basemul_x_256_32bit_2x2(M_32+256); // v x
-
-  ntt256_16bit(fh_16, fh);
-  ntt256_32bit(fh_32, fh);
-  ntt256_16bit(gh_16, gh);
-  ntt256_32bit(gh_32, gh);
-
-  basemul256_16bit_2x2(V, M_16, fh_16); // ux * fh
-  basemul256_16bit_2x2(tmp_16_1, M_16+128, gh_16); // vx * gh
-  basemul256_32bit_2x2(tmp_32_0, M_32, fh_32); // ux * fh
-  basemul256_32bit_2x2(tmp_32_1, M_32+256, gh_32); // vx * gh
-
+void gf_polymul_128x128_2x2_x2p2 (int *V,int *M,int *fh,int *gh){
   int i, T, *X, *Y, *W;
-  for (X=V, Y=tmp_16_1, i=128; i>0; i--) {
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(V);
 
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-
-  crt256(V, tmp_32_0);
-
-  basemul256_16bit_2x2(V+128, M_16+256, fh_16); // r * fh
-  basemul256_16bit_2x2(tmp_16_1, M_16+384, gh_16); // s * gh
-  basemul256_32bit_2x2(tmp_32_0, M_32+512, fh_32); // r * fh
-  basemul256_32bit_2x2(tmp_32_1, M_32+768, gh_32); // s * gh
-
-  for (X=V+128, Y=tmp_16_1, i=128; i>0; i--) {
-    T = __SADD16(*(Y++),*X);
-    *(X++) = T;
-  }
-  intt256_16bit(V+128);
-
-  for (X=tmp_32_0, Y=tmp_32_1, i=256; i>0; i--) {
-    T = *(Y++) + *X;
-    *(X++) = T;
-  }
-  intt256_32bit(tmp_32_0);
-
-  crt256(V+128, tmp_32_0);
-
-  for (X=V, Y=M, i=64; i>0; i--) {  // + f'
+  B256_1[0] = V[0] = 0;
+  gf_polymul_128x128(BB256_1, M+128, fh); 	// x * u * fh
+  gf_polymul_128x128((int*)((void*)V+2), M+192, gh);	// x * v * gh
+  for (X=V, Y=B256_1, W=M, i=64; i>0; i--) {// x(u fh+v gh)+f1
+    T = barrett_16x2i(__SADD16(__SADD16(*(W++),*(Y++)),*X)); *(X++) = T;
+  }  
+  for (i=64; i>0; i--) {  
     T = barrett_16x2i(__SADD16(*(Y++),*X)); *(X++) = T;
   } 
-  for (X=V+128, Y=M+64, i=64; i>0; i--) {  // + g'
-    T = barrett_16x2i(__SADD16(*(Y++),*X)); *(X++) = T;
+  gf_polymul_128x128(V+128, M+256, fh);	// r * fh
+  gf_polymul_128x128(BB256_1, M+320, gh);	// s * gh
+  for (Y=BB256_1, i=64; i>0; i--) {	// x(r fh+s gh) + g1
+    T = barrett_16x2i(__SADD16(__SADD16(*(W++),*(Y++)),*X)); *(X++) = T;
+  } 
+  for (i=64; i>0; i--) {  
+    T = barrett_16x2i(__SADD16(*X, *(Y++))); *(X++) = T;
+  } 
+}
+
+void gf_polymul_128x128_2x2_x_2x2 (int *M, int *M1, int *M2) {
+  int i, T, *X, *Y;
+
+  B256_1[0] = 0;
+  gf_polymul_128x128(BB256_1, M2, M1); 	// x * u2 * u1
+  gf_polymul_128x128(M, M2+64, M1+128); 	// v2 * r1
+  for (i=128, X=M, Y=B256_1; i>0; i--) {	// u = x u2 u1 + v2 r1
+    T = barrett_16x2i(__SADD16(*X,*(Y++))); *(X++) = T;
+  }
+  gf_polymul_128x128(BB256_1, M2, M1+64); 	// x * u2 * v1
+  gf_polymul_128x128(M+128, M2+64, M1+192); 	// v2 * s1
+  for (i=128, Y=B256_1; i > 0; i--) {	// v = x u2 v1 + v2 s1
+    T = barrett_16x2i(__SADD16(*X,*(Y++))); *(X++) = T;
+  }
+  gf_polymul_128x128(BB256_1, M2+128, M1); 	// x * r2 * u1
+  gf_polymul_128x128(M+256, M2+192, M1+128); 	// s2 * r1
+  for (i=128, Y = B256_1; i > 0; i--) {	// s = x r2 u1 + s2 r1
+    T = barrett_16x2i(__SADD16(*X,*(Y++))); *(X++) = T;
+  }
+  gf_polymul_128x128(BB256_1, M2+128, M1+64); 	// x * r2 * v1
+  gf_polymul_128x128(M+384, M2+192, M1+192); 	// s2 * s1
+  for (i=128, Y = B256_1; i > 0; i--) {	// s = x r2 v1 + s2 s1
+    T = barrett_16x2i(__SADD16(*X,*(Y++))); *(X++) = T;
   }
 }
 
 int jump256divsteps(int minusdelta, int *M, int *f, int *g){
-  int M1[384], M2[384], fg[256];
-  int M1_16[512]={0}, M1_32[1024];
-  int M2_16[512]={0}, M2_32[1024];
-
+int M1[384], M2[384], fg[256];
   minusdelta = jump128divsteps(minusdelta, M1, f, g);
-
-  ntt256_16bit(M1_16, M1+128); // u1
-  ntt256_16bit(M1_16+128, M1+192); // v1
-  ntt256_16bit(M1_16+256, M1+256); // r1
-  ntt256_16bit(M1_16+384, M1+320); // s1
-
-  ntt256_32bit(M1_32, M1+128); // u1
-  ntt256_32bit(M1_32+256, M1+192); // v1
-  ntt256_32bit(M1_32+512, M1+256); // r1
-  ntt256_32bit(M1_32+768, M1+320); // s1
-
-  __gf_polymul_128x128_2x2_x2p2(fg, M1_16, M1_32, f+64, g+64, M1);
-
+  gf_polymul_128x128_2x2_x2p2 (fg, M1, f+64, g+64);
   minusdelta = jump128divsteps(minusdelta, M2, fg, fg+128);
-
-  ntt256_16bit(M2_16, M2+128); // u2
-  ntt256_16bit(M2_16+128, M2+192); // v2
-  ntt256_16bit(M2_16+256, M2+256); // r2
-  ntt256_16bit(M2_16+384, M2+320); // s2
-
-  ntt256_32bit(M2_32, M2+128); // u2
-  ntt256_32bit(M2_32+256, M2+192); // v2
-  ntt256_32bit(M2_32+512, M2+256); // r2
-  ntt256_32bit(M2_32+768, M2+320); // s2
-
-  __gf_polymul_128x128_2x2_x_2x2(M+256, M1_16, M1_32, M2_16, M2_32);
-  __gf_polymul_128x128_2x2_x2p2(M, M2_16, M2_32, fg+64, fg+192, M2);
-
+  gf_polymul_128x128_2x2_x2p2 (M, M2, fg+64, fg+192);
+  gf_polymul_128x128_2x2_x_2x2(M+256, M1+128, M2+128);
   return(minusdelta);
 }
