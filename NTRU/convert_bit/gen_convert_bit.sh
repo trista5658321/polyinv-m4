1822783926752e3362c426e3433154ad2c2d44ab
Trista Li <trista5658321@gmail.com>
2021-07-13 20:50:43 +0800

    gen convert bit for NTRU509

--- NTRU/convert_bit/gen_convert_bit.sh
+++ NTRU/convert_bit/gen_convert_bit.sh
@@ -2,3 +2,7 @@
 
 python b16to_4.py 32 > convert_bit_to_4_32.S
 python b4to_16.py 32 > convert_bit_to_16_32.S
+
+python b16to_4.py 512 > ntru509/convert_bit_to_4_512.S
+python b4to_16.py 512 > ntru509/convert_bit_to_16_512.S
+python b4to_16.py 544 > ntru509/convert_bit_to_16_544.S
